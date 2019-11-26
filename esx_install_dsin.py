943196cf905f5b68732550b765bbc270d182fd3f
@tgratton <tg@ac-amiens.fr>
2019-11-26 16:47:51 +0100

    fix #1

--- esx_install_dsin.py
+++ esx_install_dsin.py
@@ -16,7 +16,7 @@
 ##
 # Synthaxe / utilisation - attention aux threads paralleles
 #
-# fab <fonction> (-P -z <nombre instances en parallele> - option P seule conseillee) -f matice_systools.py
+# fab <fonction> (-P -z <nombre instances en parallele> - option P seule conseillee) -f <script>.py
 #
 #
 
@@ -34,12 +34,8 @@ env.skip_bad_hosts=False
 env.timeout=30
 env.shell = "/bin/sh -c"
 
-#
-# INSTALLATION ESXi
-# prerequs  :
-#   - avoir installed ESXi
-#   - avoir modified le vSwitch0 en vSwitch1 dans /etc/vmware/esx.cfg
-#
+
+# ROLE AJOUT VLAN
 
 @roles('')
 
@@ -50,9 +46,15 @@ def ajout_vlan():
       abort_on_prompts=False
       ):
 
-#            run('esxcli network vswitch standard portgroup add -p Adriatic -v vSwitch0 && esxcli network vswitch standard portgroup set --vlan-id=901 --portgroup-name="Adriatic"')
-#            run('esxcli network vswitch standard portgroup set --vlan-id=901 --portgroup-name="Adriatic"')
-#            run('esxcli network vswitch standard portgroup list | grep vSwitch0 | grep 901')
+            run('esxcli network vswitch standard portgroup add -p Adriatic -v vSwitch0 && esxcli network vswitch standard portgroup set --vlan-id=901 --portgroup-name="Adriatic"')
+            run('esxcli network vswitch standard portgroup set --vlan-id=901 --portgroup-name="Adriatic"')
+            run('esxcli network vswitch standard portgroup list | grep vSwitch0 | grep 901')
+
+
+# ROLE INSTALLATION ESXi
+# prerequis  :
+#   - avoir installed ESXi
+#   - avoir modified le vSwitch0 en vSwitch1 dans /etc/vmware/esx.cfg
 
 
 @roles('esx2install')
@@ -64,68 +66,76 @@ def esxfreshinstall():
         abort_on_prompts=False
         ):
 
-            #
+
             # VARIABLES A TUNER
-            #
+
             ip_hb = '172.26.178.194'
             ip_vmotion = '192.168.230.55'
-            #
+
             ip_iscsi1 = '172.26.178.195'
             ip_iscsi2 = '172.26.178.196'
             ip_iscsi3 = '172.26.178.197'
             ip_iscsi4 = '172.26.178.198'
-            #
+
             serveurdns1 = '172.30.176.100'
             serveurdns2 = '172.30.176.245'
             serveurntp = '172.30.176.100'
             serveurlog = '172.30.176.6'
 
-            # sup shell warnings
+            # SUPPRESSION DES SHELLS WARNINGS
             run('esxcli system settings advanced set -i 1 -o /UserVars/SuppressShellWarning')
 
-            # prompt hostname
+            # PROMPT DU NOM DE L'ESX
             hostname = prompt('Saisir le nom d ESX (en miniscules) : ')
+
+            # REGLAGE HOSTNAME
             run('esxcli system hostname set --host='+hostname+'')
+
+            # REGLAGE DNS
             run('esxcli network ip dns server add -s '+serveurdns1+'')
             run('esxcli network ip dns server add -s '+serveurdns2+'')
             run('esxcli network ip dns search add -d "in.ac-amiens.fr"')
             run('esxcli network ip dns search add -d "ac-amiens.fr"')
-            # n')
+
+            # CONFIG NTP (a fixer - envoyer un fichier deja formate)
             run('rm /etc/ntp.conf')
             put('/home/tom/.bin/DSIN-AMS/ESX/ntp.conf','/etc/')
             run('sed -i -e "s/xxx/'+serveurntp+'/g" /etc/ntp.conf')
             run('chmod 644 /etc/ntp.conf && chmod -t /etc/ntp.conf')
             run('/sbin/chkconfig ntpd on')
-            #run('/etc/init.d/ntpd stop')
-            #run('/etc/init.d/ntpd start')
-            # sn')
+
+            # CONFIG SNMP
             run('esxcli system snmp set --communities public')
             run('esxcli system snmp set --enable true')
-            # sysl')
+
+            # CONFIG SYSLOG
             run('esxcli system syslog config set --loghost="'+serveurlog+'"')
             run('esxcli system syslog reload')
-            # firewa')
+
+            # CONFIG FIREWALL
             run('esxcli network firewall ruleset set --ruleset-id=syslog --enabled=true')
             run('esxcli network firewall refresh')
-            # vswitc')
+
+            # AJOUT VSWITCH0
             run('esxcli network vswitch standard add -v vSwitch0')
-            # attribution vmnic vswitc')
+
+            # ATTRIBUTION VMNIC AU SWITCH1
             #run('esxcli network vswitch standard uplink add -u vmnic2 -v vSwitch1')
             #run('esxcli network vswitch standard uplink add -u vmnic3 -v vSwitch1')
             run('esxcli network vswitch standard policy failover set -a vmnic2,vmnic3 -v vSwitch1')
-            # attribution vmnic vswitc')
+
+            # ATTRIBUTION VMNIC AU SWITCH0
             run('esxcli network vswitch standard uplink add -u vmnic0 -v vSwitch0')
             run('esxcli network vswitch standard uplink add -u vmnic1 -v vSwitch0')
             run('esxcli network vswitch standard policy failover set -a vmnic0,vmnic1 -v vSwitch0')
 
-            # desactivation du fcoe sur les interfaces
+            # DESACTIVATION DU FCOE SUR LES INTERFACES
             run('esxcli fcoe nic disable -n=vmnic0')
             run('esxcli fcoe nic disable -n=vmnic1')
             run('esxcli fcoe nic disable -n=vmnic2')
             run('esxcli fcoe nic disable -n=vmnic3')
 
-            # ajout portgroup
-            # BOUCLER SUR UNE LISTE ARRAY
+            # AJOUT DES PORTGROUPS
             run('esxcli network vswitch standard portgroup add -p mgtswitch -v vSwitch1')
             run('esxcli network vswitch standard portgroup add -p VMkernel -v vSwitch1')
             run('esxcli network vswitch standard portgroup add -p interco_forti_cp -v vSwitch1')
@@ -150,7 +160,7 @@ def esxfreshinstall():
             run('esxcli network vswitch standard portgroup add -p Agriates -v vSwitch0')
             run('esxcli network vswitch standard portgroup add -p "Racine 172.30.191.0" -v vSwitch0')
 
-            # Set the vlan id for the given portgroup
+            # CONFIG VLAN-ID POUR TOUS LES PORTGROUPS AJOUTES
             run('esxcli network vswitch standard portgroup set --vlan-id=1 --portgroup-name="mgtswitch"')
             run('esxcli network vswitch standard portgroup set --vlan-id=4 --portgroup-name="VMkernel"')
             run('esxcli network vswitch standard portgroup set --vlan-id=5 --portgroup-name="interco_forti_cp"')
@@ -175,18 +185,14 @@ def esxfreshinstall():
             run('esxcli network vswitch standard portgroup set --vlan-id=900 --portgroup-name="Agriates"')
             run('esxcli network vswitch standard portgroup set --vlan-id=910 --portgroup-name="Racine 172.30.191.0"')
 
+            # DEFINITION DU TYPE DE MULTIPATH - NMP : Native Multipathing Plugin - SATP : Storage Array Type Plugin - VMW_SATP_EQL : SATP pour Dell EqualLogic iSCSI
             run('esxcli storage nmp satp set --default-psp=VMW_PSP_RR --satp=VMW_SATP_EQL')
 
-            # anciennement esxcfg-vswitch --mtu=9000 ${VS:-vSwitch1}
+            # REGLAGE MTU VSWITCH1 (anciennement esxcfg-vswitch --mtu=9000 ${VS:-vSwitch1})
             run('esxcli network vswitch standard set -m 9000 -v vSwitch1')
 
-            # ancienncement esxcfg-vswitch --set-cdp both ${VS:-vSwitch1}
+            # REGLAGE AUTRE VSWITCH1 (ancienncement esxcfg-vswitch --set-cdp both ${VS:-vSwitch1})
             run('esxcli network vswitch standard set -c both -v vSwitch1')
-
-            # ADRESSAGE ESX ET STOCKAGE
-            #
-            # AUTOMATISER LA RECUPERATION DES IP
-            #
 
             # HB ISCSI
             run('esxcfg-vmknic -a -i '+ip_hb+' -n 255.255.255.0 --mtu=9000 VMkernel-iscsi-heartbeat')
@@ -196,44 +202,42 @@ def esxfreshinstall():
             run('esxcfg-vmknic -a -i '+ip_vmotion+' -n 255.255.255.224 VMkernel')
             run('esxcli network vswitch standard portgroup set --portgroup-name=VMkernel --vlan-id=4')
 
-            # VOIR OU LES AJOUTER
-            
-            #esxcli network vswitch standard portgroup policy failover set --active-uplinks=vmnic2 --portgroup-name="iSCSI-1"
-            #esxcli network vswitch standard portgroup policy failover set --active-uplinks=vmnic3 --portgroup-name="iSCSI-2"
-            #esxcli network vswitch standard portgroup policy failover set --active-uplinks=vmnic2 --portgroup-name="iSCSI-3"
-            #esxcli network vswitch standard portgroup policy failover set --active-uplinks=vmnic3 --portgroup-name="iSCSI-4"
-
-            #
+            # AJOUT ISCSI1 AU SWITCH1
             run('esxcfg-vswitch -A iSCSI-1 vSwitch1')
             run('esxcfg-vmknic -a -i '+ip_iscsi1+' -n 255.255.255.0 -m 9000 iSCSI-1')
             run('esxcli network vswitch standard portgroup set --portgroup-name=iSCSI-1 --vlan-id=78')
             run('esxcfg-vswitch -p iSCSI-1 -N vmnic3 vSwitch1')
 
-            #
+            # AJOUT ISCSI2 AU SWITCH1
             run('esxcfg-vswitch -A iSCSI-2 vSwitch1')
             run('esxcfg-vmknic -a -i '+ip_iscsi2+' -n 255.255.255.0 -m 9000 iSCSI-2')
             run('esxcli network vswitch standard portgroup set --portgroup-name=iSCSI-2 --vlan-id=78')
             run('esxcfg-vswitch -p iSCSI-2 -N vmnic2 vSwitch1')
 
-            #
+            # AJOUT ISCSI3 AU SWITCH1
             run('esxcfg-vswitch -A iSCSI-3 vSwitch1')
             run('esxcfg-vmknic -a -i '+ip_iscsi3+' -n 255.255.255.0 -m 9000 iSCSI-3')
             run('esxcli network vswitch standard portgroup set --portgroup-name=iSCSI-3 --vlan-id=78')
             run('esxcfg-vswitch -p iSCSI-3 -N vmnic3 vSwitch1')
 
-            #
+            # AJOUT ISCSI4 AU SWITCH1
             run('esxcfg-vswitch -A iSCSI-4 vSwitch1')
             run('esxcfg-vmknic -a -i '+ip_iscsi4+' -n 255.255.255.0 -m 9000 iSCSI-4')
             run('esxcli network vswitch standard portgroup set --portgroup-name=iSCSI-4 --vlan-id=78')
             run('esxcfg-vswitch -p iSCSI-4 -N vmnic2 vSwitch1')
 
-            # recuperer vmkx via listing vmkernel appelled VMkernel
+            # VOIR OU LES AJOUTER
+            
+            #esxcli network vswitch standard portgroup policy failover set --active-uplinks=vmnic2 --portgroup-name="iSCSI-1"
+            #esxcli network vswitch standard portgroup policy failover set --active-uplinks=vmnic3 --portgroup-name="iSCSI-2"
+
+            # RECUPERATION VMK VIA LISTING VMKERNEL
             run('vim-cmd hostsvc/vmotion/vnic_set vmk2')
 
             # AJOUT DES EQL AU DATASTORE
-            #
             run('esxcli iscsi software set -e true')
             run('esxcli iscsi adapter list')
+
             # RECUPERATION NOM VMHBA APRES AVOIR RESCAN
             vmhba = run("esxcfg-scsidevs -a | awk ' /iscsi_vmk/ { print $1 }'")
             run('esxcli iscsi networkportal add -n vmk3 -A '+vmhba+'')
@@ -241,32 +245,26 @@ def esxfreshinstall():
             run('esxcli iscsi networkportal add -n vmk5 -A '+vmhba+'')
             run('esxcli iscsi networkportal add -n vmk6 -A '+vmhba+'')
 
-            #
+            # AJOUT EQL1
             run('esxcli iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.31')
             run('esxcli iscsi adapter discovery sendtarget auth chap set --direction=uni --authname=CLUSTERLAME-ESX --secret=chap-CLUSTERLAME-ESX --level=preferred -A '+vmhba+' --address=172.26.178.31')
-            run('esxcli iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.47')
-            run('esxcli iscsi adapter discovery sendtarget auth chap set --direction=uni --authname=CLUSTERLAME-ESX --secret=chap-CLUSTERLAME-ESX --level=preferred -A '+vmhba+' --address=172.26.178.47')
-            run('esxcli iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.60')
-            run('esxcli iscsi adapter discovery sendtarget auth chap set --direction=uni --authname=CLUSTERLAME-ESX --secret=chap-CLUSTERLAME-ESX --level=preferred -A '+vmhba+' --address=172.26.178.60')
-            run('esxcli iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.6')
-            run('esxcli iscsi adapter discovery sendtarget auth chap set --direction=uni --authname=CLUSTERLAME-ESX --secret=chap-CLUSTERLAME-ESX --level=preferred -A '+vmhba+' --address=172.26.178.6')
 
-            #
-            run('esxcli storage core adapter rescan --adapter='+vmhba+'')
-
-            #
+            # AJOUT EQL5
             run('esxcli iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.158')
-            #run('esxcli iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.159')
-            run('esxcli iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.183')
-            #run('esxcli iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.184')
-            run('esxcli iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.206')
-            run('vmhba iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.219')
             run('esxcli iscsi adapter discovery sendtarget auth chap set --direction=uni --authname=EQL5-ESX --secret=chap-EQL5-ESX --level=preferred -A '+vmhba+' --address=172.26.178.158')
-            #run('esxcli iscsi adapter discovery sendtarget auth chap set --direction=uni --authname=EQL6-ESX --secret=chap-EQL6-ESX --level=preferred -A '+vmhba+' --address=172.26.178.159')
+
+            # AJOUT EQL7
+            run('esxcli iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.183')
             run('esxcli iscsi adapter discovery sendtarget auth chap set --direction=uni --authname=EQL7-ESX --secret=chap-EQL7-ESX --level=preferred -A '+vmhba+' --address=172.26.178.183')
-            #run('esxcli iscsi adapter discovery sendtarget auth chap set --direction=uni --authname=EQL8-ESX --secret=chap-EQL8-ESX --level=preferred -A '+vmhba+' --address=172.26.178.184')
+
+            # AJOUT EQL9
+            run('esxcli iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.206')
             run('esxcli iscsi adapter discovery sendtarget auth chap set --direction=uni --authname=EQL9-ESX --secret=chap-EQL9-ESX --level=preferred -A '+vmhba+' --address=172.26.178.206')
+
+            # AJOUT EQL10
+            run('vmhba iscsi adapter discovery sendtarget add -A '+vmhba+' --address=172.26.178.219')
             run('esxcli iscsi adapter discovery sendtarget auth chap set --direction=uni --authname=EQL10-ESX --secret=chap-EQL10-ESX --level=preferred -A '+vmhba+' --address=172.26.178.219')
-            #
+
+            # RESCAN DE LA CHAINE ISCSI
             run('esxcli storage core adapter rescan --adapter='+esxcli+'')
 
